5afec3dad62b339b39107e9b01ae5fcd2a77dd99
GoktugYmsk <gogtug033@outlook.com>
2024-02-18 14:42:00 +0300

    added mock cities

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
 import "./index.scss";
 
 function Content() {
-  return <div>Content</div>;
+  return (
+    <div className="container-content">
+      <div>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Content from "../content";
+import Header from "../header";
 import "./index.scss";
 
 function Layout() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
 }
 
 export default Layout;
